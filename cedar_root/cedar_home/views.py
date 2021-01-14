5e001a72ba06cdc0bf59ae063c488d6e9c378bcf
Brennan Chapman <brennan@chapmanb.com>
2021-01-14 10:07:40 -0500

    Add personalization to index, and complete CSS from Bootstrap template.

--- cedar_root/cedar_home/views.py
+++ cedar_root/cedar_home/views.py
@@ -7,3 +7,12 @@ def index(request):
     title = "Hello World"
     page_title = "CEDAR"
     return render(request, 'cedar_home/index.html', {'title': title, 'page_title': page_title})
+
+    """ 
+    Working w. vars
+
+    def index(request):
+    title = "Hello World"
+    page_title = "CEDAR"
+    return render(request, 'cedar_home/index.html', {'title': title, 'page_title': page_title}) 
+    """
