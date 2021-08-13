cdf7e5435cf449e51d5a9914bc05854ae50783ca
Brennan Chapman <brennan@chapmanb.com>
2021-08-13 10:29:26 -0400

    Update configuration from azure-django app

--- cedar_root/manage.py
+++ cedar_root/manage.py
@@ -5,8 +5,10 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cedar_site.settings')
+    
+    settings_module = "cedar_site.production" if 'WEBSITE_HOSTNAME' in os.environ else 'cedar_site.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
