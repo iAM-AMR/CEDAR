6c3af2c2ce78454e77337eb4130957b778ef9c39
Brennan Chapman <brennan@chapmanb.com>
2022-05-28 19:38:24 -0400

    Recreate Pipfile.lock, updated for Python 3.10.4 with Django 4.0.4. Change call to url() to re_path. '
    url() is not available in Django 4+.

--- cedar_root/cedar_core/urls.py
+++ cedar_root/cedar_core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path, reverse
+from django.urls import include, path, reverse, re_path
 
 from . import views
 
@@ -6,11 +6,9 @@ from cedar_core.views import PublisherAutocomplete
 
 from django.contrib.auth import views as auth_views
 
-from django.conf.urls import url
-
 urlpatterns = [
     ##path('accounts/', include('django.contrib.auth.urls')), #create_field='publish-id-autocomplete'
-    url(r'^publish-id-autocomplete/$', PublisherAutocomplete.as_view(), name='publish-id-autocomplete',),
+    re_path(r'^publish-id-autocomplete/$', PublisherAutocomplete.as_view(), name='publish-id-autocomplete',),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='cedar_core/login.html')),
     path('references/', views.view_references, name='view_references'),
     path('references/<int:ref_id>/', views.ref_detail, name='ref_detail'),
