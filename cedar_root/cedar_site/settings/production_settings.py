43943277e5915ad0ae5b8b1756253ec87639a926
Brennan Chapman <brennan@chapmanb.com>
2023-01-31 15:57:50 -0500

    Deploy in DEBUG

--- cedar_root/cedar_site/settings/production_settings.py
+++ cedar_root/cedar_site/settings/production_settings.py
@@ -23,7 +23,7 @@ except ImportError as error_production_import_settings_base:
 # Failing to do so will result in all requests being returned as “Bad Request (400)”.
 # See https://docs.djangoproject.com/en/4.1/ref/settings/#allowed-hosts
 
-DEBUG = [os.environ['DEBUG']] == 'TRUE'
+DEBUG = True
 
 
 # We set WEBSITE_HOSTNAME on Azure App Service. If missing, set an empty
