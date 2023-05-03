a782b8f2aec8e5db203501ccc76905cd4b7258c6
Brennan Chapman <chapb@users.noreply.github.com>
2023-05-02 20:48:56 -0400

    Document reference_filter

--- cedar_root/cedar_core/filters.py
+++ cedar_root/cedar_core/filters.py
@@ -3,21 +3,29 @@ from cedar_core.models import reference
 import django_filters
 
 
+
 class reference_filter(django_filters.FilterSet):
     
-    
-    MYCHOICE = {
+    """
+    Filter references shown at Browse References.
+    """
 
-       
+    # By default for a Boolean, django-filter displays a drop-down with 
+    # "Unknown", "True", and 'False'. To customize these choices, we 
+    # must manually define a choice mapping, and specify a ChoiceFilter. 
+    # The null value is specified in the ChoiceFilter.
+
+    THECHOICES = {
         (True, 'Excluded'),
         (False, 'Included')
     }
     
-    is_excluded = django_filters.ChoiceFilter(label='Exclusion Status', field_name='is_excluded_extract', choices=MYCHOICE, empty_label='Show All')
+    is_excluded = django_filters.ChoiceFilter(
+        label='Exclusion Status', 
+        field_name='is_excluded_extract', 
+        choices=THECHOICES, 
+        empty_label='Show All')
 
-
-
-    
     class Meta:
         model = reference
         fields = ['is_excluded']
