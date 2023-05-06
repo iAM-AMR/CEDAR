f9dcc9f24198297b693bd16fed2548ca2b4d0039
Brennan Chapman <chapb@users.noreply.github.com>
2023-05-06 11:09:03 -0400

    Add resistance_class and resistance filters
    - Add resistance_class and resistance filters.
    - Add custom layout by specifying individual fields.
    - Document filters.py

--- cedar_root/cedar_core/filters.py
+++ cedar_root/cedar_core/filters.py
@@ -1,7 +1,12 @@
 
-from cedar_core.models import reference, res_outcome, host_01, microbe_01
+"""
+The filters.py file enables django_filters.
+"""
 
 import django_filters
+from cedar_core.models import (host_01, microbe_01, reference, res_outcome)
+
+
 
 
 
@@ -33,6 +38,8 @@ class reference_filter(django_filters.FilterSet):
 
 
 
+
+
 class timber_filter(django_filters.FilterSet):
     
     """
@@ -40,18 +47,39 @@ class timber_filter(django_filters.FilterSet):
     """
 
     # Unclear if this is the best implementation. Also unlear if queryset target is appropriate.
-    host = django_filters.ModelMultipleChoiceFilter(field_name = 'factor__host_level_01__host_01_name', 
-                                                    queryset=host_01.objects.all(), 
-                                                    label = "Host",
-                                                    )
+    host    = django_filters.ModelMultipleChoiceFilter(field_name = 'factor__host_level_01__host_01_name', 
+                                                       queryset=host_01.objects.all(), 
+                                                       label = "Host",)
     
     # Unclear if this is the best implementation. Also unlear if queryset target is appropriate.
     microbe = django_filters.ModelMultipleChoiceFilter(field_name = 'microbe_level_01__microbe_01_name', 
                                                        queryset=microbe_01.objects.all(), 
-                                                       label = "Microbe")
+                                                       label = "Microbe",)
+    
+    
+    # MultipleChoiceFilter() requires a choice list structured as (value, 'label').
+    # We generate a choice list based on the contents of res_outcomes, rather than 
+    # the full atc_vet list, to avoid cluttering the UI with unavailable choices.
+    # The simplest way to generate this list structure is to pass the same field 
+    # name twice.
+
+    resistance_class_choices = res_outcome.objects.distinct('resistance__levelname_4_coarse').values_list('resistance__levelname_4_coarse', 'resistance__levelname_4_coarse')
+
+    resistance_class         = django_filters.MultipleChoiceFilter(field_name = 'resistance__levelname_4_coarse', 
+                                                                   label      = 'Resistance Class',
+                                                                   choices    = resistance_class_choices,)
+    
+    resistance_choices       = res_outcome.objects.distinct('resistance__levelname_5').values_list('resistance__levelname_5', 'resistance__levelname_5')
+    
+    resistance               = django_filters.MultipleChoiceFilter(field_name = 'resistance__levelname_5', 
+                                                                   label      = 'Resistance',
+                                                                   choices    = resistance_choices,)
+
 
     class Meta:
         model = res_outcome
-        fields = ['host', 'microbe']
+        fields = ['host', 'microbe', 'resistance_class', 'resistance']
+
+
 
 
