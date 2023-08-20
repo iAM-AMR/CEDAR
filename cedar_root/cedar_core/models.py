17529c6ddc731c22631cd9c09c318da4ad818cf7
Brennan Chapman <chapb@users.noreply.github.com>
2023-08-20 12:12:15 -0400

    refactor: move get_help_text function to module

--- cedar_root/cedar_core/models.py
+++ cedar_root/cedar_core/models.py
@@ -3,9 +3,8 @@
 # CEDAR Models
 
 from decimal import Decimal
-from pathlib import Path
 
-import pandas
+from utils.get_help_text import get_help_text
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.core.mail import send_mail
@@ -17,40 +16,7 @@ from django.utils.translation import gettext_lazy as _
 
 from .validators import UnicodeUsernameValidator
 
-# Read in the local CEDAR_dictionary to get captions and help-text.
 
-BASE_DIR               = Path(__file__).resolve().parent.parent
-cedar_forest_dict_path = BASE_DIR / 'CEDAR_forest_dictionary.csv'
-cedar_forest_dict      = pandas.read_csv(cedar_forest_dict_path, encoding='utf-8')
-
-# Create lists from each column.
-field_names    = cedar_forest_dict.field.tolist()
-field_helpt    = cedar_forest_dict.description.tolist()
-field_captions = cedar_forest_dict.caption.tolist()
-
-# Make dictionaries indexed by field names.
-dict_help = dict(zip(field_names, field_helpt))
-dict_capt = dict(zip(field_names, field_captions))
-
-
-
-# Clean the data dict
-for key in dict_help:
-    if isinstance(dict_help[key], float):
-        dict_help[key] = ''
-    else:
-        dict_help[key] = dict_help[key].replace('\xa0',' ')
-
-# Clean the data dict
-for key in dict_capt:
-    if isinstance(dict_capt[key], float):
-        dict_capt[key] = ''
-    else:
-        dict_capt[key] = dict_capt[key].replace('\xa0',' ')
-
-
-def get_help_text(field_name, dict = dict_help):
-    return(dict.get(field_name, "Missing Help Text"))
 
 
 
